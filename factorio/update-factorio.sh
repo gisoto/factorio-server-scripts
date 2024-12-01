7073d6c4d18118effee70ec7e24bbe90470bcf53
Giovanni Soto <gio@Gios-MacBook-Pro.local>
2024-12-01 02:16:35 -0500

    Created function for docker compose commands that recreate container

--- factorio/update-factorio.sh
+++ factorio/update-factorio.sh
@@ -21,10 +21,19 @@ echo "Pulling the latest image version."
 docker compose --file "$script_dir"/docker-compose.yml pull --quiet
 new_image_id=$(docker inspect --format "{{.Id}}" factoriotools/factorio:stable)
 
+# callable only after container_name is defined and the latest image has been pulled
+docker_compose_restart() {
+  echo "Recreating container $container_name with the latest image version."
+  docker compose --file "$script_dir"/docker-compose.yml down;
+  docker compose --file "$script_dir"/docker-compose.yml up --detach;
+  factorio_version=$(docker inspect --format '{{index .Config.Labels "factorio.version"}}' "$container_name")
+  echo "Factorio version: $factorio_version"
+}
+
 # check if container does not exists
 if ! docker inspect "$container_name" > /dev/null 2>&1; then
   echo "Container $container_name does not exist."
-  docker compose --file "$script_dir"/docker-compose.yml up --detach
+  docker_compose_restart
   exit 0
 fi
 echo "Container $container_name exists."
@@ -32,9 +41,7 @@ echo "Container $container_name exists."
 # check if the container is not running
 if ! docker inspect --format '{{.State.Status}}' "$container_name" | grep --quiet "running"; then
   echo "Container $container_name is not running."
-  echo "Recreating container $container_name with the latest image version."
-  docker compose --file "$script_dir"/docker-compose.yml down
-  docker compose --file "$script_dir"/docker-compose.yml up --detach
+  docker_compose_restart
   exit 0
 fi
 echo "Container $container_name is running."
@@ -58,8 +65,5 @@ if [ "$new_image_id" = "$container_image_id" ]; then
 fi
 
 # restart server with latest update
-echo "Recreating container $container_name with the latest image version."
-docker compose --file "$script_dir"/docker-compose.yml down;
-docker compose --file "$script_dir"/docker-compose.yml up --detach;
-factorio_version=$(docker inspect --format '{{index .Config.Labels "factorio.version"}}' "$container_name")
-echo "Factorio version: $factorio_version"
+docker_compose_restart
+exit 0
